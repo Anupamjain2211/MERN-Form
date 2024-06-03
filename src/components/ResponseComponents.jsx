60205ac0ce3e3ade58c0562077397747ea105ec5
anupam <anupamjain177@gmail.com>
2024-06-03 15:15:17 -0400

    updated

---
+++ src/components/ResponseComponents.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ResponseComponents = () => {
+  return (
+    <div id='resDiv'>
+      <h3>Name : <span className='dynamicValues'>Anupam Jain</span></h3>
+      <h3>Email : <span className='dynamicValues'>AnupamJain@gmail.com</span></h3>
+      <h3>Phone : <span className='dynamicValues'>8897677867</span></h3>
+      <h3>Message : <span className='dynamicValues'>Hello Anupam </span></h3>
+    </div>
+  )
+}
+
+export default ResponseComponents
--- src/pages/Responses.jsx
+++ src/pages/Responses.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ResponseComponents from '../components/ResponseComponents'
+import "./Responses.css"
 
 const Responses = () => {
   return (
-    <div>
-      res
+    <div id='maincontainer'>
+      <ResponseComponents/>
+      <ResponseComponents/>
+      <ResponseComponents/>
+      <ResponseComponents/>
+      <ResponseComponents/>
     </div>
   )
 }
